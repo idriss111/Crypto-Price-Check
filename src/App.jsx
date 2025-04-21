f9b60a0535fad840edfd516395066c30ad1c9bcc
El Qoraychy Driss <idrissqr1@gmail.com>
2025-04-22 01:19:48 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CryptoChecker from './CryptoChecker';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+      <CryptoChecker />
+    </div>
+  );
+}
+
+export default App;
